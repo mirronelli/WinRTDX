896f738d953d666f18e30ce39021936807108b2e
Miroslav Moravek <miroslav.moravek@2ring.com>
2020-09-20 20:14:16 +0200

    added support for map/unmap resource updating in constant buffers

--- WinRTDX/PSConstantBuffer.h
+++ WinRTDX/PSConstantBuffer.h
@@ -9,27 +9,28 @@ namespace Dx::Attachables
 	class PSConstantBuffer : public Attachable
 	{
 	public:
-		static std::shared_ptr<PSConstantBuffer<T>> Create(uint16_t key, bool overwrite, std::shared_ptr<Graphics> graphics, T& constantData, UINT slot = 0)
+		static std::shared_ptr<PSConstantBuffer<T>> Create(uint16_t key, bool overwrite, std::shared_ptr<Graphics> graphics, T& constantData, UINT slot = 0, bool fastMode = true)
 		{
 			std::shared_ptr<PSConstantBuffer<T>> instance = std::static_pointer_cast<PSConstantBuffer<T>>(ResourceManager::PSConstantBuffers[key]);
 
 			if (overwrite || instance == nullptr)
 			{
-				instance = std::make_shared<PSConstantBuffer>(key, graphics, constantData, slot);
+				instance = std::make_shared<PSConstantBuffer>(key, graphics, constantData, slot, fastMode);
 				ResourceManager::PSConstantBuffers[key] = instance;
 			}
 
 			return instance;
 		}
 
-		PSConstantBuffer(uint16_t key, std::shared_ptr<Graphics> graphics, T& constantData, UINT slot)
+		PSConstantBuffer(uint16_t key, std::shared_ptr<Graphics> graphics, T& constantData, UINT slot, bool fastMode)
 			: Attachable(key, graphics),
-			m_slot(slot)
+			m_slot(slot),
+			m_fastMode(fastMode)
 		{
 			D3D11_BUFFER_DESC	desc{ 0 };
 			desc.BindFlags = D3D11_BIND_FLAG::D3D11_BIND_CONSTANT_BUFFER;
-			desc.Usage = D3D11_USAGE::D3D11_USAGE_DEFAULT;
-			desc.CPUAccessFlags = 0;
+			desc.Usage = fastMode ? D3D11_USAGE::D3D11_USAGE_DYNAMIC : D3D11_USAGE::D3D11_USAGE_DEFAULT;
+			desc.CPUAccessFlags = fastMode ? D3D11_CPU_ACCESS_FLAG::D3D11_CPU_ACCESS_WRITE : 0;
 			desc.ByteWidth = sizeof(T);
 
 			D3D11_SUBRESOURCE_DATA srd{ 0 };
@@ -50,11 +51,20 @@ namespace Dx::Attachables
 
 		void Update(T const& constantData)
 		{
-			m_context->UpdateSubresource(m_buffer.get(), 0, 0, &constantData, 0, 0);
+			if (m_fastMode)
+			{ 
+				D3D11_MAPPED_SUBRESOURCE subresource;
+				m_context->Map(m_buffer.get(), 0, D3D11_MAP::D3D11_MAP_WRITE_DISCARD, 0, &subresource);
+				memcpy(subresource.pData, &constantData, sizeof(constantData));
+				m_context->Unmap(m_buffer.get(), 0);
+			}
+			else
+				m_context->UpdateSubresource(m_buffer.get(), 0, 0, &constantData, 0, 0);
 		}
 
 	private:
 		com_ptr<ID3D11Buffer>	m_buffer;
 		UINT							m_slot;
+		bool							m_fastMode;
 	};
 }
--- WinRTDX/VSConstantBuffer.h
+++ WinRTDX/VSConstantBuffer.h
@@ -9,27 +9,28 @@ namespace Dx::Attachables
 	class VSConstantBuffer : public Attachable
 	{
 	public:
-		static std::shared_ptr<VSConstantBuffer<T>> Create(uint16_t key, bool overwrite, std::shared_ptr<Graphics> graphics, T& constantData, UINT slot = 0)
+		static std::shared_ptr<VSConstantBuffer<T>> Create(uint16_t key, bool overwrite, std::shared_ptr<Graphics> graphics, T& constantData, UINT slot = 0, bool fastMode = true)
 		{
 			std::shared_ptr<VSConstantBuffer<T>> instance = std::static_pointer_cast<VSConstantBuffer<T>>(ResourceManager::VSConstantBuffers[key]);
 
 			if (overwrite || instance == nullptr)
 			{
-				instance = std::make_shared<VSConstantBuffer>(key, graphics, constantData, slot);
+				instance = std::make_shared<VSConstantBuffer>(key, graphics, constantData, slot, fastMode);
 				ResourceManager::VSConstantBuffers[key] = instance;
 			}
 
 			return instance;
 		}
 
-		VSConstantBuffer(uint16_t key, std::shared_ptr<Graphics> graphics, T& constantData, UINT slot)
+		VSConstantBuffer(uint16_t key, std::shared_ptr<Graphics> graphics, T& constantData, UINT slot, bool fastMode)
 			: Attachable(key, graphics),
-			m_slot(slot)
+			m_slot(slot),
+			m_fastMode(fastMode)
 		{
 			D3D11_BUFFER_DESC	desc{ 0 };
 			desc.BindFlags = D3D11_BIND_FLAG::D3D11_BIND_CONSTANT_BUFFER;
-			desc.Usage = D3D11_USAGE::D3D11_USAGE_DEFAULT;
-			desc.CPUAccessFlags = 0;
+			desc.Usage = fastMode ? D3D11_USAGE::D3D11_USAGE_DYNAMIC : D3D11_USAGE::D3D11_USAGE_DEFAULT;
+			desc.CPUAccessFlags = fastMode ? D3D11_CPU_ACCESS_FLAG::D3D11_CPU_ACCESS_WRITE : 0;
 			desc.ByteWidth = sizeof(T);
 
 			D3D11_SUBRESOURCE_DATA srd{ 0 };
@@ -50,11 +51,20 @@ namespace Dx::Attachables
 
 		void Update(T const& constantData)
 		{
-			m_context->UpdateSubresource(m_buffer.get(), 0, 0, &constantData, 0, 0);
+			if (m_fastMode)
+			{
+				D3D11_MAPPED_SUBRESOURCE subresource;
+				m_context->Map(m_buffer.get(), 0, D3D11_MAP::D3D11_MAP_WRITE_DISCARD, 0, &subresource);
+				memcpy(subresource.pData, &constantData, sizeof(constantData));
+				m_context->Unmap(m_buffer.get(), 0);
+			}
+			else
+				m_context->UpdateSubresource(m_buffer.get(), 0, 0, &constantData, 0, 0);
 		}
 
 	private:
 		com_ptr<ID3D11Buffer>	m_buffer;
 		UINT							m_slot;
+		bool							m_fastMode;
 	};
 }
